d9b304b1d5f4d229d720e59037f7a865ce4d6633
Benjamin Eriksen <benja.e95@gmail.com>
2020-12-30 18:46:58 +0100

    Notebook update: M2 finished'ish

--- code/main.py
+++ code/main.py
@@ -1,4 +1,4 @@
-from plotting import plot_64, plot_64_m2, plot_loss_m1 ,plot_loss_m2, plot_interpolation, make_cm
+from plotting import plot_64, plot_64_m2, plot_loss_m1 ,plot_loss_m2, plot_interpolation_m1, plot_interpolation_m2, make_cm
 from models import VAE, M1, M2
 from training import run_epochs
 from datasets import importMNIST, importFashionMNIST
@@ -9,7 +9,7 @@ import torch
 
 ## Settings
 batch_size = 512
-epochs = 2
+epochs = 20
 latent_dim = 8
 learning_rate = 1e-3
 image_channels = 1
@@ -77,7 +77,7 @@ elif mode=="m1":
 
     # sample model and plot interpolations
     z_in=model.sample(batch_size=10000,z_out=True)
-    plot_interpolation(model, z_in=z_in,image_n=0,latent_dim=latent_dim,imsize=28,interpolate_dim=18,std_=3,batch_size=10000)
+    plot_interpolation_m1(model, z_in=z_in,image_n=0,latent_dim=latent_dim,imsize=28,interpolate_dim=18,std_=3,batch_size=10000)
 
     # classifier
     kneighbors_classifier(model,dset_train, dset_test, classes)
@@ -96,15 +96,15 @@ elif mode=="m2":
     # plot reconstructions
     plot_64_m2(model=model, sample=True, y=(torch.ones(64).type(torch.int64)*9))
 
-    # Confusion_matrixes
+    # confusion_matrixes
     print("Mean acc (train): ", train_acc[-1])
-    make_cm(dset_train)
+    make_cm(model, dset_train)
     print("Mean acc (val): ", test_acc[-1])
-    make_cm(dset_test)
+    make_cm(model, dset_test)
 
     # sample model and plot interpolations
     z_in = model.sample(y=torch.Tensor([0,1,2,3,4,5,6,7,8,9]).type(torch.int64))["z"]
-    plot_interpolation(model, z_in=z_in,image_n=0,latent_dim=latent_dim,imsize=28,interpolate_dim=18,std_=5,batch_size=10000)
+    plot_interpolation_m2(model, z_in=z_in,image_n=0,latent_dim=latent_dim,imsize=28,interpolate_dim=18,std_=5,batch_size=10000)
 
 else:
     print("Unknown mode")
--- code/plotting.py
+++ code/plotting.py
@@ -1,6 +1,9 @@
-import matplotlib.pyplot as plt
 import torch
+import sklearn
 import numpy as np
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
 from loaders import get_target_indexes
 classes = [0,1,2,3,4,5,6,7,8,9]
 
@@ -66,7 +69,7 @@ def plot_64_m2(model=None,batch=None,sample=False,data=None,y=torch.arange(len(c
     plt.show()
 
 
-def plot_interpolation(model,z_in=None,image_n=0,latent_dim=8,imsize=28,interpolate_dim=20,std_=3,batch_size=100):
+def plot_interpolation_m1(model,z_in=None,image_n=0,latent_dim=8,imsize=28,interpolate_dim=20,std_=3,batch_size=100):
   if z_in is None:
       ztest =model.sample(batch_size=batch_size,z_out=True)
   else:
@@ -81,7 +84,7 @@ def plot_interpolation(model,z_in=None,image_n=0,latent_dim=8,imsize=28,interpol
 
   for c in range(latent_dim*interpolate_dim):
       test_data[c][int(c/interpolate_dim)]=range_[int(c/interpolate_dim)][c%interpolate_dim]
-      # print(c,int(c/interpolate_dim),"_",int(c/interpolate_dim),c%interpolate_dim,"_",range_[int(c/interpolate_dim)][c%interpolate_dim])
+      # print(c,int(c/interpolate   _dim),"_",int(c/interpolate_dim),c%interpolate_dim,"_",range_[int(c/interpolate_dim)][c%interpolate_dim])
       # print(c,test_data[c].cpu().detach().numpy())
   smple_pic =model.decode(test_data.to(model.device))
 
@@ -96,6 +99,34 @@ def plot_interpolation(model,z_in=None,image_n=0,latent_dim=8,imsize=28,interpol
   plt.imshow(all_pics, interpolation='none')
   plt.show()
   return(ztest.std(0).cpu().detach().numpy())
+
+def plot_interpolation_m2(model, z_in=None,image_n=0,latent_dim=8,imsize=28,interpolate_dim=20,std_=3,batch_size=100):
+  if z_in is None:
+      ztest =model.sample(batch_size=batch_size,z_out=True)
+  else:
+    ztest=z_in
+  test_data=torch.zeros(latent_dim*interpolate_dim,latent_dim)
+  for i in range(test_data.shape[0]):
+      test_data[i]=ztest[image_n]
+  range_=np.zeros([latent_dim,interpolate_dim])
+  for c,i in enumerate(ztest.std(0).cpu().detach().numpy()):
+      range_[c]=np.linspace(-std_*i,std_*i,int(interpolate_dim))
+  for c in range(latent_dim*interpolate_dim):
+      test_data[c][int(c/interpolate_dim)]=range_[int(c/interpolate_dim)][c%interpolate_dim]
+      # print(c,int(c/interpolate_dim),"_",int(c/interpolate_dim),c%interpolate_dim,"_",range_[int(c/interpolate_dim)][c%interpolate_dim])
+      # print(c,test_data[c].cpu().detach().numpy())
+  smple_pic =model.decode(test_data.to(model.device),y=torch.ones(144).type(torch.int64)*5)
+  all_pics=np.zeros([imsize*latent_dim,imsize*interpolate_dim])
+  for i in range(latent_dim):
+      for j in range(interpolate_dim):
+  #         print(i,j)
+          all_pics[i*imsize:(i+1)*imsize,j*imsize:(j+1)*imsize]=smple_pic[(interpolate_dim*i)+j][0].cpu().data
+  fig = plt.figure()
+  fig.set_figheight(40)
+  fig.set_figwidth(20)
+  plt.imshow(all_pics, interpolation='none')
+  return(ztest.std(0).cpu().detach().numpy())
+
 
 def plot_loss_m1(train_loss,test_loss):
     x_epoch = np.arange(len(train_loss))
@@ -123,14 +154,14 @@ def plot_loss_m2(train_loss,test_loss, train_acc, test_acc ):
     plt.show()
 
 
-def make_cm(dset):
+def make_cm(model, dset):
   idx_test_subset = get_target_indexes(dset, classes, 10)
   X_test = dset.data[idx_test_subset].type(torch.float)
   y_test = dset.targets[idx_test_subset].numpy()
 
   model.eval()
   with torch.no_grad():
-      y_pred = model.classifier(X_test.to(device)).cpu().numpy()
+      y_pred = model.classifier(X_test.to(model.device)).cpu().numpy()
       y_pred = np.argmax(y_pred, axis=1)
 
   cm = sklearn.metrics.confusion_matrix(y_true = y_test, y_pred = y_pred)
